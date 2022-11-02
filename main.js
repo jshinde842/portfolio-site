894da551cfe3531dc3b07820a4c046ab3ee6617f
Jay Shinde <jshinde@Jays-MBP.attlocal.net>
2022-11-02 13:01:44 -0700

    disabled button

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 if(!window.location.hash) {
     location.hash = '#home';
 }
+function pageDisabledAlert() {
+    alert("This Page is a Work-In-Progress");
+  }
 document.getElementById("disabled").onclick = function(){alert("This Page is a Work-In-Progress");};
