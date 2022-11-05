a9b683ea9e2ee36a5b6a1d8db31f2f4fb6a21eed
Jay Shinde <jshinde@Jays-MBP.attlocal.net>
2022-11-05 00:11:11 -0700

    project page

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ if(!window.location.hash) {
 function pageDisabledAlert() {
     alert("This Page is a Work-In-Progress");
 }
+
+//f1 project github link button
+const f1repoBtn = document.getElementById('f1repo');
+f1repoBtn.addEventListener('click', () => {
+    window.open('https://github.com/jshinde842/Formula-1-Standings-Predictions', '_blank')
+});
