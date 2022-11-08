11b91d3fb066c30ee640fbec88d0d5ce5a0b177c
Jay Shinde <jshinde@Jays-MBP.attlocal.net>
2022-11-07 18:31:07 -0800

    mobile menu

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ function pageDisabledAlert() {
     alert("This page is still under construction and cannot be accessed.");
 }
 
+function mobileDropdownFunction() {
+    var x = document.getElementById("mobileLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
