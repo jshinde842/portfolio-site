5b0bb6837c3328fe7a1d6951f9b9ff16e9154283
Jay Shinde <jshinde@Jays-MBP.attlocal.net>
2022-11-13 01:20:59 -0800

    project page

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ function pageDisabledAlert() {
     alert("This page is still under construction and cannot be accessed.");
 }
 
+function repoDisabledAlert() {
+    alert("This is a private repository. Please contact me for access.");
+}
+
 function mobileDropdownFunction() {
     var x = document.getElementById("mobileLinks");
     if (x.style.display === "block") {
